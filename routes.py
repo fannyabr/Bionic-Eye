e13c58faf191bd4c8942c5e1802aaecf0d62cd9a
fanny abramovich <funnyabr@gmail.com>
2021-03-18 16:08:00 +0200

    implementing Factory

---
+++ routes.py
@@ -0,0 +1,26 @@
+from flask import Response, jsonify
+from flask import current_app as app
+import os
+from BionicEye.controllers.video_controller import add_video, get_video_paths
+
+
+@app.route('/addVideo', methods=['POST'])
+def run_add_video():
+    try:
+        add_video()
+    except:
+        Response().set_data("Couldn't save video")
+
+    return Response()
+
+
+@app.route('/videoPaths', methods=['GET'])
+def run_get_video_paths():
+    video_paths = get_video_paths()
+
+    return jsonify(video_paths)
+
+
+if __name__ == '__main__':
+    os.makedirs('videos', exist_ok=True)
+    app.run()
